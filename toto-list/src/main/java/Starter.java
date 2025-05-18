fa07558912870e6141ed1d59d1162dc94a3262a8
Imantha-Kodikara <imantha.l.kodikara@gmail.com>
2025-05-18 09:22:34 +0530

    scene created

--- toto-list/src/main/java/Starter.java
+++ toto-list/src/main/java/Starter.java
@@ -1,2 +1,16 @@
-public class Starter {
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/todo_list_view.fxml"))));
+        stage.show();
+    }
 }
