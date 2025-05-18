fef9e564d6bd792a42bc1e8a3fcd90b41254f509
Imantha-Kodikara <imantha.l.kodikara@gmail.com>
2025-05-18 10:01:02 +0530

    tasks with checkboxes and add functions added

--- toto-list/src/main/java/controller/ToDoController.java
+++ toto-list/src/main/java/controller/ToDoController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
 public class ToDoController {
+
+    @FXML
+    private ListView<CheckBox> listTask;
+
+    @FXML
+    private TextField txtEnterTask;
+
+    ObservableList<CheckBox>tasks = FXCollections.observableArrayList();
+    @FXML
+    void btnAddTaskOnClick(ActionEvent event) {
+        String taskText = txtEnterTask.getText();
+        CheckBox checkBox = new CheckBox(taskText);
+        tasks.add((checkBox));
+        listTask.setItems(tasks);
+        txtEnterTask.clear();
+
+    }
+
 }
